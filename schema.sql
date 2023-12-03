143555958ef244430c756a9da0317137e20c4226
ChengchengCai006 <145884793+ChengchengCai006@users.noreply.github.com>
2023-12-03 15:34:40 -0500

    Update annotation and schema.sql
    1. Add explanation for part 1&2
    2. Add annotation for function
    3. Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,31 +1,37 @@
 -- first table: NYC_311
-CREATE TABLE IF NOT EXISTS NYC_311 (
+CREATE TABLE IF NOT EXISTS nyc_311 (
    unique_key INTEGER PRIMARY KEY,
    created_date TIMESTAMP,
    complaint_type TEXT,
    zipcode INTEGER,
-   location Geometry('POINT', srid=4326)
+   location geometry(Point,4326) 
 );
 
+CREATE INDEX IF NOT EXISTS idx_nyc_311_location ON nyc_311 USING GIST(location);
+
 -- second table: NYC_tree
-CREATE TABLE IF NOT EXISTS NYC_tree (
+CREATE TABLE IF NOT EXISTS nyc_tree (
    tree_id INTEGER PRIMARY KEY,
    status TEXT,
    health TEXT,
    species TEXT,
    zipcode INTEGER,
-   location Geometry('POINT', srid=4326)
+   location geometry(Point,4326)
 );
 
--- third table: NYC_zipcode
-CREATE TABLE IF NOT EXISTS NYC_zipcode (
+CREATE INDEX IF NOT EXISTS idx_nyc_tree_location ON nyc_tree USING GIST(location);
+
+-- third table: nyc_zipcode
+CREATE TABLE IF NOT EXISTS nyc_zipcode (
    zipcode INTEGER PRIMARY KEY,
-   location Geometry('POLYGON', srid=4326)
+   location geometry(POLYGON, 4326)
 );
 
--- fourth table: NYC_zillow
-CREATE TABLE IF NOT EXISTS NYC_zillow (
+CREATE INDEX IF NOT EXISTS idx_nyc_zipcode_location ON nyc_zipcode USING GIST(location);
+
+-- fourth table: nyc_zillow
+CREATE TABLE IF NOT EXISTS nyc_zillow (
    zipcode INTEGER PRIMARY KEY,
-   recorded_date DATE,
+   recorded_date Date,
    rent FLOAT
 );
